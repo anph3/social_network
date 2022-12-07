73c8b04bedade3f6fff32fa7cb90ae657b0fecf0
AnPH13 <0306191294@caothang.edu.vn>
2022-12-07 16:57:50 +0700

    update schedule

--- social_network/celery.py
+++ social_network/celery.py
@@ -1,6 +1,7 @@
 import os
 
 from celery import Celery
+from celery.schedules import crontab
 
 os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'social_network.settings')
 
@@ -11,7 +12,8 @@ app.config_from_object('django.conf:settings', namespace='CELERY')
 app.conf.beat_schedule = {
     'print_jobs_3s':{
         'task':'api_app.tasks.print_jobs',
-        'schedule': 3.0
+        'schedule': crontab(minute='*/1'),
+        'args':()
     }
 }
 
