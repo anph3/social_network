b752fb3fefbc6ab3e39c1f1fe413dd75cc1a747d
kimnt <you@example.com>
2023-01-30 16:11:07 +0700

    init app mini_game

---
+++ mini_game/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class MiniGameConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'mini_game'
---
+++ mini_game/urls.py
@@ -0,0 +1,10 @@
+from django.urls import path
+
+all_url = {
+    
+}
+
+urlpatterns = []
+
+for item in all_url:
+    urlpatterns += all_url[item]
